759f160009dd9182ddfbab8dde2fb2ba4a749561
Charity Samuel <scharity463@gmail.com>
2024-09-12 00:18:16 +0300

    initial commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Cherry Barbz on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
