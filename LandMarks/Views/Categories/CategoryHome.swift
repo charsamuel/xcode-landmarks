6dca6aaa9e0f1940727370d1a84a23d89c068e0c
Charity Samuel <scharity463@gmail.com>
2024-09-15 03:26:43 +0300

    added toggle and edit features ui controls

--- LandMarks/Views/Categories/CategoryHome.swift
+++ LandMarks/Views/Categories/CategoryHome.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
 
 
     var body: some View {
@@ -20,13 +21,27 @@ struct CategoryHome: View {
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
+               
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
         } detail: {
             Text("Select a Landmark")
         }
     }
 }
+
 
 #Preview {
     CategoryHome()
