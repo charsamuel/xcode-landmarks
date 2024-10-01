5016edc96ededf3324291ab0d8004a83bb518fc5
Charity Samuel <scharity463@gmail.com>
2024-10-01 11:57:48 +0300

    Created watchOS app

--- LandMarks/LandMarksApp.swift
+++ LandMarks/LandMarksApp.swift
@@ -1,12 +1,20 @@
 import SwiftUI
 
+
 @main
-struct LandMarksApp: App {
+struct LandmarksApp: App {
     @State private var modelData = ModelData()
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(modelData)
         }
+
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
